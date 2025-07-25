44312c593935ba1da1d350f85bd573c7f6f684a9
nursena-pc <nursenaduyku1@gmail.com>
2025-07-25 21:42:40 +0300

    ilk yükleme

--- src/main/java/com/antalyaotel/model/Role.java
+++ src/main/java/com/antalyaotel/model/Role.java
@@ -6,6 +6,7 @@ public enum Role implements GrantedAuthority {
     ROLE_USER,
     ROLE_ADMIN;
 
+
     @Override
     public String getAuthority() {
         return name(); // ✅ Spring Security'nin beklentisini karşılar
